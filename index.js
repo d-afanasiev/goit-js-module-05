5e3d23cfa7b00807b9a1266efea0a05312bf835e
Afanasiev Dmitriy <afanasiev.dimon@outlook.com>
2021-04-27 14:13:11 +0300

    hw-08

--- index.js
+++ index.js
@@ -1,25 +1,13 @@
-function StringBuilder(baseValue) {
-  this.value = baseValue;
-}
-StringBuilder.prototype.getValue = function () {
-  return this.value;
-};
-StringBuilder.prototype.padStart = function (str) {
-  return (this.value = `${str}${this.value}`);
-};
-StringBuilder.prototype.padEnd = function (str) {
-  return (this.value = `${this.value}${str}`);
-};
-StringBuilder.prototype.padBoth = function (str) {
-  return (this.value = `${str}${this.value}${str}`);
-};
+// function User() {
+//   // Тело функции-конструктора
+// }
 
-// Пиши код выше этой строки
-const builder = new StringBuilder(".");
-console.log(builder.getValue()); // '.'
-builder.padStart("^");
-console.log(builder.getValue()); // '^.'
-builder.padEnd("^");
-console.log(builder.getValue()); // '^.^'
-builder.padBoth("=");
-console.log(builder.getValue()); // '=^.^='
+// const mango = new User();
+
+// class User {
+//   // Тело класса
+// }
+
+// const mango = new User();
+
+class Car {}
