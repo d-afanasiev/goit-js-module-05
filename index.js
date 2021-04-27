5244314819de32b780e71f894a9088bec2d60c7a
Afanasiev Dmitriy <afanasiev.dimon@outlook.com>
2021-04-27 11:51:24 +0300

    hw-05

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
+// function User() {}
+// console.log(User.prototype); // { constructor: User }
+
+// const mango = new User();
+// console.log(User.prototype.isPrototypeOf(mango)); // true
+
 // function User({ name, email }) {
 //   this.name = name;
 //   this.email = email;
 // }
 
-// const mango = new User({ name: "Манго", email: "mango@mail.com" });
-// const poly = new User({ name: "Поли", email: "poly@mail.com" });
+// User.prototype.getEmail = function () {
+//   return this.email;
+// };
+
+// User.prototype.changeEmail = function (newEmail) {
+//   this.email = newEmail;
+// };
+
+// const mango = new User({ name: 'Манго', email: 'mango@mail.com' });
+
+// console.log(mango.getEmail()); // mango@mail.com
+// mango.changeEmail('mango@supermail.com');
+// console.log(mango.getEmail()); // mango@supermail.com
 
 function Car({ brand, model, price }) {
   this.brand = brand;
@@ -12,6 +29,16 @@ function Car({ brand, model, price }) {
   this.price = price;
 }
 
+Car.prototype.getPrice = function () {
+  return this.price;
+};
+
+Car.prototype.changePrice = function (newPrice) {
+  return (this.price = newPrice);
+};
+
 const newCar = new Car({ brand: "Audi", model: "Q3", price: 36000 });
 
-console.log(newCar);
+console.log(newCar.getPrice());
+
+console.log(newCar.changePrice(35000));
