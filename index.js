b291125b59e6cdb16045374c6a49e13f6eac515d
Afanasiev Dmitriy <afanasiev.dimon@outlook.com>
2021-04-27 14:17:42 +0300

    hw-09

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
-// function User() {
-//   // Тело функции-конструктора
+// function User({ name, email }) {
+//   this.name = name;
+//   this.email = email;
 // }
-
-// const mango = new User();
 
 // class User {
-//   // Тело класса
+//   // Необязательное объявление публичных свойств
+//   name;
+//   email;
+
+//   // Аналог функции-конструктора
+//   constructor({ name, email }) {
+//     // Инициализация объявленных свойств
+//     this.name = name;
+//     this.email = email;
+//   }
 // }
 
-// const mango = new User();
+// const mango = new User({ name: "Манго", email: "mango@mail.com" });
 
-class Car {}
+class Car {
+  brand;
+  model;
+  price;
+
+  constructor({ brand, model, price }) {
+    this.brand = brand;
+    this.model = model;
+    this.price = price;
+  }
+}
