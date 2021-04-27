3e6c2d02aaa6eb6c1dbd0d2d8cc8acf64de7d8ed
Afanasiev Dmitriy <afanasiev.dimon@outlook.com>
2021-04-27 11:42:36 +0300

    hw-04

--- index.js
+++ index.js
@@ -1,30 +1,17 @@
-// function User() {
-//   // Тело функции
-// }
-
-// const mango = new User();
-// console.log(mango); // {}
-
-// const poly = new User();
-// console.log(poly); // {}
-
-// function User(name, email, age) {
+// function User({ name, email }) {
 //   this.name = name;
 //   this.email = email;
 // }
 
-// const mango = new User('Манго', 'mango@mail.com');
-// console.log(mango); // { name: 'Манго', email: 'mango@mail.com' }
+// const mango = new User({ name: "Манго", email: "mango@mail.com" });
+// const poly = new User({ name: "Поли", email: "poly@mail.com" });
 
-// const poly = new User('Поли', 'poly@mail.com');
-// console.log(poly); // { name: 'Поли', email: 'poly@mail.com' }
-
-function Car(brand, model, price) {
+function Car({ brand, model, price }) {
   this.brand = brand;
   this.model = model;
   this.price = price;
 }
 
-const newCar = new Car("Audi", "Q3", 36000);
+const newCar = new Car({ brand: "Audi", model: "Q3", price: 36000 });
 
 console.log(newCar);
